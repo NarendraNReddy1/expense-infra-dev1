4fdd1eb66c711423c20012880933afb7712fd817
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-09-16 03:47:54 -0700

    ALL

--- 03-bastion/provider.tf
+++ 03-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
     bucket = "daws78s-remote-state-3"
-    key    = "remote-bastion_infra_444"
+    key    = "remote-bastion_infra_456"
     region = "us-east-1"
     dynamodb_table="daws78s-locking1"
   }
